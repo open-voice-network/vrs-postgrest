e0aa3f9624ce66464ecae7e11bcdc57d8781b82d
John Trammell <John.Trammell@target.com>
2021-07-30 12:55:05 -0500

    add country to PK

--- schema.sql
+++ schema.sql
@@ -4,11 +4,14 @@ DROP TABLE IF EXISTS application;
 
 CREATE TABLE application (
   name        VARCHAR(100),
-  url         VARCHAR(100),
+  country     VARCHAR(3),
+  destination VARCHAR(100) NOT NULL,
   registered  date NOT NULL DEFAULT CURRENT_DATE,
   expires     date NOT NULL DEFAULT (CURRENT_DATE + interval '1 year'),
-  CONSTRAINT name_pk PRIMARY KEY(name)
+  CONSTRAINT name_country_pk PRIMARY KEY(name, country)
 );
+COMMENT ON COLUMN application.name is 'Application invocation name';
+COMMENT ON COLUMN application.country is 'ISO 3166-1 A3 country code'; -- see https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
 
 INSERT INTO application (name, url, registered, expires)
 VALUES
